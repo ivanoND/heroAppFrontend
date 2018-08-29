3531cb82ff19d3c7e742f18e4d7e38e0933af7ad
Fabio Napoleoni <f.napoleoni@gmail.com>
2018-08-29 15:09:35 +0200

    MessageService e MessageComponent

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,17 +2,20 @@ import {Injectable} from '@angular/core';
 import {HEROES} from './mock-heroes';
 import {Observable, of} from 'rxjs';
 import {Hero} from './hero';
+import {MessageService} from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() {
+  constructor(private messageService: MessageService) {
     console.log('Creazione di HeroService');
   }
 
   getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
