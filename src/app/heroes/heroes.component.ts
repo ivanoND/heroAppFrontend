788e2ed0a9d247ab29ef31d84b9894163f980990
Fabio Napoleoni <f.napoleoni@gmail.com>
2018-08-30 15:47:30 +0200

    Interfaccia di navigazione nel detail di singolo utente

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,9 +9,6 @@ import {MessageService} from '../message.service';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-
-  selectedHero: Hero;
-
   heroes: Hero[];
 
   // questa sintassi allo stesso tempo dichiara un campo
@@ -29,19 +26,5 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe((heroes) => this.heroes = heroes);
-  }
-
-  // in java sarebbe public void onSelect(Hero hero)
-  // noinspection TsLint
-  onSelect(hero: Hero) {
-    // sintassi ES6 per interpolazione stringe, usare il carattere
-    // backtick ` per delimitare le stringhe e l'espressione
-    // ${qualche espressione} per inserire un espressione
-    // http://geckosoft.it/blog/come-digitare-il-catattere-backtick-accento-grave-e-tilde-su-windows-utilizzando-una-tastiera-italiana-senza-tastierino-numerico
-    this.messageService.add(`Hai cliccato sull'eroe ${hero.name}`);
-    // in javascript 5 sarebbe cosi
-    // console.log('Hai cliccato sull\'eroe' + hero.name);
-    // assegno l'eroe selezionato
-    this.selectedHero = hero;
   }
 }
