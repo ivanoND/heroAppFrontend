b634e13a4a40043daef6ae5ac02f2c64011c99c7
Fabio Napoleoni <f.napoleoni@gmail.com>
2018-08-31 14:14:15 +0200

    In memory http server e modifica service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,26 +3,36 @@ import {HEROES} from './mock-heroes';
 import {Observable, of} from 'rxjs';
 import {Hero} from './hero';
 import {MessageService} from './message.service';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) {
+  // URL to web api
+  private heroesUrl = 'api/heroes';
+
+  constructor(private http: HttpClient,
+              private messageService: MessageService) {
     messageService.add('Creazione di HeroService');
   }
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    this.log('HeroService: fetched heroes');
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   // public Observable<Hero> getHero(int id)
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    this.log(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
